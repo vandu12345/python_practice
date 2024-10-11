c7ad83cece73e59547d6d5b8fe9d6b2e2093d5ea
vandu12345 <suvendumahapatra2@gmail.com>
2024-10-11 03:54:17 +0000

    Added list,tuple

---
+++ ds.py
@@ -0,0 +1,50 @@
+# a = [1,1,1,2,3,4]
+
+# print(set(a))
+
+# result = {}
+
+# result[5] = 2
+
+# result[5] = 3
+
+# k->v
+
+
+# print(result)
+
+# for element in a:
+#     if element in result:
+#         result[element] +=1
+#     else:
+#         result[element] = 1
+        
+
+# for element in a:
+#     result[element] = result.get(element, 0) + 1
+
+
+# k -> v
+
+# for k,v  in result.items():
+#     print(f"{k} - > {v}")
+    
+    
+    
+# a,b,c = 5,6,7
+
+# print(result)
+
+
+def abc():
+    return 5,6
+
+d, e = abc()
+print(type(d))
+
+# d[1] = 6
+
+a = "test"
+
+       
+    
